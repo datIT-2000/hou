c6d3f7c39e5efd4c04cb5e733aa3cc1f4f77bb0c
Trịnh Xuân Đạt <84932547+datIT-2000@users.noreply.github.com>
2021-06-05 22:26:31 +0700

    Create Chup_anh.cpp

---
+++ Contest/XX-21.01/Chup_anh.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin ("photo.in");
+    ofstream cout ("photo.out");
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> a(n-1);
+    for(auto &i:a) cin >> i;
+    for(int i=1;i<=a[0];++i){
+        set<int> s;
+        vector<int> res;
+        s.insert(i);
+        res.push_back(i);
+        for(int j=0;j<a.size();++j){
+            if(a[j]>res.back()){
+                s.insert(a[j]-res.back());
+                res.push_back(a[j]-res.back());
+            }
+            else{
+                break;
+            }
+        }
+        if(s.size()==n){
+            for(int i=0;i<res.size();++i) 
+                cout << res[i] << (i<res.size()-1?' ':'\n');
+            return 0;   
+        }
+    }
+}
+ 
